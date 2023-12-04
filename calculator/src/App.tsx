92514169b98e4206464e8bacf76cade8bb1691c0
Sonia Lavandera <lavanderasonia@outlook.es>
2023-12-04 22:21:56 +0100

    feat(project): Add template vite + react

---
+++ calculator/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Calculator from '@/components/Calculator';
+
+function App() {
+
+  return (
+    <div className='app'>
+      <h1>Calculator</h1>
+      <Calculator/>
+    </div>
+  )
+}
+
+export default App
